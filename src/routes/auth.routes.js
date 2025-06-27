ca10d3ef175a84ea8fc373687067b52e35fafed8
Hermes <hermesbarbosa9@gmail.com>
2025-06-27 16:33:25 -0300

    configurar midlleware de autenticação

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const { registerUser, login } = require("../controllers/auth.controllers")
+const { registerUser, login } = require("../controllers/auth.controllers");
+const { authenticateToken } = require("../middlewares/authMiddleware");
 
 router.post("/register", registerUser)
 router.post("/login", login)
